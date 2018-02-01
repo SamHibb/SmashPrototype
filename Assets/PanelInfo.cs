d0301bc3a6ac899864aae380a2ef888193724f3e
Huw Tingley <huw.tingley@gmail.com>
2018-02-01 09:36:54 +0000

    UI should now load sprites

--- Assets/PanelInfo.cs
+++ Assets/PanelInfo.cs
@@ -12,6 +12,11 @@ public class PanelInfo : MonoBehaviour
     public Text player3Text;
     public Text player4Text;
 
+    public Sprite player1Image;
+    public Sprite player2Image;
+    public Sprite player3Image;
+    public Sprite player4Image;
+
     private void Start()
     {
         players = GameObject.FindGameObjectsWithTag("Player");
@@ -28,21 +33,25 @@ public class PanelInfo : MonoBehaviour
         {
             player1Text.text = "Damage: " + players[0].GetComponent<Damage>().getDamageMul()
                 + "\nDeaths: " + players[0].GetComponent<Damage>().getTimesDied();
+            player1Image = players[0].GetComponent<Image>().sprite;
         }
         if (players.Length > 1)
         {
             player2Text.text = "Damage: " + players[1].GetComponent<Damage>().getDamageMul()
                  + "\nDeaths: " + players[1].GetComponent<Damage>().getTimesDied();
+            player2Image = players[1].GetComponent<Image>().sprite;
         }
         if (players.Length > 2)
         {
             player3Text.text = "Damage: " + players[2].GetComponent<Damage>().getDamageMul()
                  + "\nDeaths: " + players[2].GetComponent<Damage>().getTimesDied();
+            player3Image = players[2].GetComponent<Image>().sprite;
         }
         if (players.Length > 3)
         {
             player4Text.text = "Damage: " + players[3].GetComponent<Damage>().getDamageMul()
-                 + "\nDeaths: " + players[3].GetComponent<Damage>().getTimesDied(); 
+                 + "\nDeaths: " + players[3].GetComponent<Damage>().getTimesDied();
+            player4Image = players[3].GetComponent<Image>().sprite;
         } 
     }
 }
