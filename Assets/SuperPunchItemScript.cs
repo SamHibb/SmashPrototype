4af55686a357661c730477f714c0939b6fc88503
Huw Tingley <huw.tingley@gmail.com>
2018-02-05 00:55:30 +0000

    added superpunch item- although doesn't work as i dont' understand the attack script

---
+++ Assets/SuperPunchItemScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperPunchItemScript : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<Attack>().ChangeStrength(1000);
+        Destroy(gameObject);
+    }
+}
